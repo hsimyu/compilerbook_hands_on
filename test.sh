e4b7101734803ba2c009e53fefca1cb61aefcba4
hsimyu <hsimyu1900@gmail.com>
2023-09-04 00:18:06 +0900

    Add test.sh.

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./bazel-bin/mycc/mycc "$input" > asm/tmp.s
+    cc -o outputs/tmp asm/tmp.s
+    ./outputs/tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
