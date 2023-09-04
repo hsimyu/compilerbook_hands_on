a25d8f79083365dbf7ab2494793722d02559295f
hsimyu <hsimyu1900@gmail.com>
2023-09-05 00:03:52 +0900

    Impl Parser definitions.

--- mycc/main.c
+++ mycc/main.c
@@ -156,6 +156,93 @@ Token *tokenize(char *p)
     return head.next;
 }
 
+// parser
+typedef enum
+{
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// AST のノード
+struct Node
+{
+    NodeKind kind; // 種類
+    Node *lhs;     // 左辺
+    Node *rhs;     // 右辺
+    int val;       // kind が ND_NUM の場合のみ
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr()
+{
+    // expr = mul ("+" mul | "-" mul)*
+    Node *node = mul();
+
+    for (;;)
+    {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul()
+{
+    // mul = primary ("*" primary | "/" primary)*
+    Node *node = primary();
+
+    for (;;)
+    {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary()
+{
+    // primary = num | "(" expr ")"
+    if (consume('('))
+    {
+        Node *node = expr();
+        expect(')'); // () で囲まれていなければエラー
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
